0dd3f4d2e98bb40120a77d696d5d51de2bebcb32
Tony Thompson <subnautican@gmail.com>
2022-12-02 15:07:58 -0600

    logic for changing the height, the width, and using the multiplier implimented

--- controller.py
+++ controller.py
@@ -1,24 +1,60 @@
 from PyQt5.QtWidgets import *
 from functions import *
-from gui import *
+from view import *
+
+
+def clean_input(value):
+    try:
+        value = float(value)
+    except ValueError:
+        return 0
+    else:
+        return value
 
 
 class Controller(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
-        self.button_go.clicked.connect(lambda: self.calculate())
+        self.lineEdit_width.textEdited.connect(lambda: self.calc_from_width())
+        self.lineEdit_height.textEdited.connect(lambda: self.calc_from_height())
+        self.pushButton_multiplier.clicked.connect(lambda: self.multiply())
 
-    def calculate(self):
-        try:
-            ratio_width = int(self.aspect_width.text())
-            ratio_height = int(self.aspect_height.text())
-            value_width = float(self.input_width.text())
-            value_height = float(self.input_height.text())
-        except ValueError:
-            self.label_output.setText(
-                'broken')
-        except:
-            self.label_output.setText('error')
+    def calc_from_width(self):
+        self.label_output.setText("")
+        val_width = clean_input(self.lineEdit_width.text())
+        val_aspect_width = clean_input(self.lineEdit_aspect_width.text())
+        val_aspect_height = clean_input(self.lineEdit_aspect_height.text())
+        if val_width == 0 or val_aspect_width == 0 or val_aspect_height == 0:
+            self.label_output.setText("Please enter non-zero values")
+            return
         else:
-            self.label_output.setText('result')
+            val_height = height_from_width(
+                val_width, val_aspect_width, val_aspect_height
+            )
+            self.lineEdit_height.setText(f'{val_height}')
+
+    def calc_from_height(self):
+        self.label_output.setText("")
+        val_height = clean_input(self.lineEdit_height.text())
+        val_aspect_width = clean_input(self.lineEdit_aspect_width.text())
+        val_aspect_height = clean_input(self.lineEdit_aspect_height.text())
+        if val_height == 0 or val_aspect_width == 0 or val_aspect_height == 0:
+            self.label_output.setText("Please enter non-zero values")
+            return
+        else:
+            val_width = width_from_height(
+                val_height, val_aspect_width, val_aspect_height
+            )
+            self.lineEdit_width.setText(f'{val_width}')
+
+    def multiply(self):
+        val_multiplier = clean_input(self.lineEdit_multiplier.text())
+        val_height = clean_input(self.lineEdit_height.text())
+        val_width = clean_input(self.lineEdit_width.text())
+        if val_width == 0 or val_height == 0 or val_multiplier == 0:
+            self.label_output.setText("Please enter non-zero values")
+            return
+        else:
+            self.lineEdit_height.setText(f'{val_multiplier * val_height}')
+            self.lineEdit_width.setText(f'{val_multiplier * val_width}')
--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-def width_from_height(h, ratio_h, ratio_w):
-    return
+def width_from_height(h, ratio_w, ratio_h):
+    return h / (ratio_h / ratio_w)
 
 
-def height_from_width(w, ratio_h, ratio_w):
-    return
+def height_from_width(w, ratio_w, ratio_h):
+    return w * (ratio_h / ratio_w)

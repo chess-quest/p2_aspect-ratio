c8a1f60fda829d134f460985a51d1f8df5fc5506
Tony Thompson <subnautican@gmail.com>
2022-12-08 19:10:14 -0600

    cleaned up main

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from controller import *
-import sys
 
-#code for adding in tracebacks, del later
-def catch_exceptions(t, val, tb):
-    QtWidgets.QMessageBox.critical(None,
-                                   "An exception was raised",
-                                   "Exception type: {}".format(t))
-    old_hook(t, val, tb)
-
-old_hook = sys.excepthook
-sys.excepthook = catch_exceptions
 
 def main():
     app = QApplication([])
